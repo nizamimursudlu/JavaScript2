766d41aaa5c751ccc5596d5cbc495e12df5c5702
Nizami Mursudlu <nizamimursudlu@gmail.com>
2020-12-08 20:44:39 +0100

    corrections after Samir's feedback

--- Week1/homework/project/app.js
+++ Week1/homework/project/app.js
@@ -1,35 +1,35 @@
 const objectWithQuotes = [{
-  Quote: "Life is what happens when you're busy making other plans.",
-  Author: "- John Lennon"
+  quote: "Life is what happens when you're busy making other plans.",
+  author: "- John Lennon"
 },
 {
-  Quote: "The way to get started is to quit talking and begin doing.",
-  Author: "- Walt Disney"
+  quote: "The way to get started is to quit talking and begin doing.",
+  author: "- Walt Disney"
 },
 {
-  Quote: "If life were predictable it would cease to be life, and be without flavor.",
-  Author: "- Eleanor Roosevelt"
+  quote: "If life were predictable it would cease to be life, and be without flavor.",
+  author: "- Eleanor Roosevelt"
 },
 {
-  Quote: "It is during our darkest moments that we must focus to see the light.",
-  Author: "- Aristotle"
+  quote: "It is during our darkest moments that we must focus to see the light.",
+  author: "- Aristotle"
 },
 {
-  Quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
-  Author: "- Benjamin Franklin"
+  quote: "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+  author: "- Benjamin Franklin"
 },
 {
-  Quote: "Whoever is happy will make others happy too.",
-  Author: "- Anne Frank"
+  quote: "Whoever is happy will make others happy too.",
+  author: "- Anne Frank"
 }]
 
 const newQuoteBtn = document.getElementById("new-quote-btn");
-let chosenQuote = document.querySelector("#new-quote");
-let chosenAuthor = document.querySelector("#author > input");
+const chosenQuote = document.querySelector("#new-quote");
+const chosenAuthor = document.querySelector("#author > input");
 function quotePicker() {
-  let quoteNum = Math.floor((Math.random()) * 6)
-  let newQuote = objectWithQuotes[quoteNum]
-  chosenQuote.value = newQuote.Quote
-  chosenAuthor.value = newQuote.Author
+  const quoteNum = Math.floor((Math.random()) * 6)
+  const newQuote = objectWithQuotes[quoteNum]
+  chosenQuote.value = newQuote.quote
+  chosenAuthor.value = newQuote.author
 }
 newQuoteBtn = addEventListener("click", quotePicker)
