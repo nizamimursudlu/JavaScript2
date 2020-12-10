2e5f277e426022932bfffa059590208065d99781
Nizami Mursudlu <nizamimursudlu@gmail.com>
2020-12-10 13:23:05 +0100

    exercise 02 done

--- Week3/js-exercises/ex2-RemoveDuplicates.js
+++ Week3/js-exercises/ex2-RemoveDuplicates.js
@@ -9,27 +9,25 @@
 
  */
 
-/**
- * Checks your solution against correct solution
- * @param {Array} array your solution
- * @returns boolean
- */
-function checkSolution(array) {
-  const solution = ['a', 'b', 'c', 'd', 'e', 'f'];
-  if (array == null) return false;
-  if (array.length !== solution.length) return false;
 
-  for (let i = 0; i < solution.length; i++) {
-    if (array[i] !== solution[i]) return false;
-  }
-  return true;
-}
 
 // WRITE YOUR FUNCTION HERE
 
-const letters = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c', 'b'];
-removeDuplicates(letters);
+const letters = ['a', 'b', 'b', 'c', 'd', 'a', 'e', 'f', 'f', 'c', 'b'];
+const alphabeticalOrder = letters.sort()
+const newArray = [];
 
-if (checkSolution(letters)) {
-  console.log("Hooray!");
+for (i = 0; i < alphabeticalOrder.length; i++) {
+  if (alphabeticalOrder[i] !== alphabeticalOrder[i + 1]) {
+    newArray.push(alphabeticalOrder[i])
+  }
 }
+
+console.log(newArray)
+
+
+
+
+
+
+
