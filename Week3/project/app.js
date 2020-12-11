8c9bf5f1a329c499f16f88127f02459f2afbd737
Nizami Mursudlu <nizamimursudlu@gmail.com>
2020-12-12 00:37:18 +0100

    finished calculation part of the project

--- Week3/project/app.js
+++ Week3/project/app.js
@@ -1,3 +1,33 @@
-// Your code goes in here
 
-document.querySelector("#app").innerText = "Tip Calculator";
+const bill = document.getElementById("bill");
+const dropdown = document.getElementById("dropdown");
+const sharingBill = document.getElementById("sharing-bill")
+const calculate = document.getElementById("calculate")
+
+
+
+
+calculate.addEventListener("click", calculation)
+
+if (bill.value == 3) {
+  console.log("it works")
+}
+
+
+function calculation() {
+  var money = parseFloat(dropdown.options[dropdown.selectedIndex].value)
+  const tips = (bill.value / 100) * money
+  total.value = (parseFloat(bill.value) + tips) / (parseFloat(sharingBill.value)) + " EACH"
+
+}
+
+
+
+
+
+
+
+
+
+
+//document.querySelector("#app").innerText = "Tip Calculator";
