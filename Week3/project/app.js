8d5979120eb2da3bfccd3b1bb04ca3e89b689f83
Nizami Mursudlu <nizamimursudlu@gmail.com>
2020-12-13 00:54:57 +0100

    modal box is finished

--- Week3/project/app.js
+++ Week3/project/app.js
@@ -1,29 +1,32 @@
 
 const bill = document.getElementById("bill");
 const dropdown = document.getElementById("dropdown");
-const sharingBill = document.getElementById("sharing-bill")
-const calculate = document.getElementById("calculate")
+const sharingBill = document.getElementById("sharing-bill");
+const calculate = document.getElementById("calculate");
+const modalBtn = document.getElementById("modal-btn");
+const modalBg = document.querySelector(".modal-bg");
+const container = document.getElementById("container")
 
 
+calculate.addEventListener("click", function () {
+  if (bill.value === "" || dropdown.value === "Choose an option" || sharingBill.value === "") {
+    modalBg.classList.add("bg-active");
+    container.style.backgroundColor = "rgb(0, 0, 0, 0.01)";
+    bill.style.backgroundColor = "rgb(0, 0, 0, 0.01)";
+
+  } else {
+    var money = parseFloat(dropdown.options[dropdown.selectedIndex].value)
+    const finalBill = (parseFloat(bill.value) + (bill.value / 100) * money) / (parseFloat(sharingBill.value))
+    total.value = `TIP AMOUNT $ ${finalBill} EACH`
+
+  }
+})
 
 
-calculate.addEventListener("click", calculation)
-
-if (bill.value == 3) {
-  console.log("it works")
-}
-
-
-function calculation() {
-  var money = parseFloat(dropdown.options[dropdown.selectedIndex].value)
-  const tips = (bill.value / 100) * money
-  total.value = (parseFloat(bill.value) + tips) / (parseFloat(sharingBill.value)) + " EACH"
-
-}
-
-
-
-
+modalBtn.addEventListener("click", function () {
+  modalBg.classList.remove("bg-active");
+  container.style.backgroundColor = "white";
+})
 
 
 
