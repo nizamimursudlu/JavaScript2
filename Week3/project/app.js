4adc96133510ce5e04eabd03202d1ab76b3ae4b6
Nizami Mursudlu <nizamimursudlu@gmail.com>
2020-12-14 00:07:01 +0100

    project is done

--- Week3/project/app.js
+++ Week3/project/app.js
@@ -1,28 +1,30 @@
 
 const bill = document.getElementById("bill");
 const dropdown = document.getElementById("dropdown");
-const sharingBill = document.getElementById("sharing-bill");
-const calculate = document.getElementById("calculate");
+const person = document.getElementById("person");
+const calculate = document.getElementById("calculate-btn");
 const modalBtn = document.getElementById("modal-btn");
 const modalBg = document.querySelector(".modal-bg");
 const container = document.getElementById("container")
-
+const tipAmount = document.getElementById("tip-amount")
 
 calculate.addEventListener("click", function () {
-  if (bill.value === "" || dropdown.value === "Choose an option" || sharingBill.value === "") {
+  if (bill.value === "" || dropdown.value === "Choose an option" || person.value === "") {
     modalBg.classList.add("bg-active");
     container.style.backgroundColor = "rgb(0, 0, 0, 0.01)";
     bill.style.backgroundColor = "rgb(0, 0, 0, 0.01)";
-
   } else {
     var money = parseFloat(dropdown.options[dropdown.selectedIndex].value)
-    const finalBill = (parseFloat(bill.value) + (bill.value / 100) * money) / (parseFloat(sharingBill.value))
-    total.value = `TIP AMOUNT $ ${finalBill} EACH`
-
+    const finalBill = (parseFloat(bill.value) + (bill.value / 100) * money) / (parseFloat(person.value))
+    if (person.value == 1) {
+      tipAmount.innerHTML = `TIP AMOUNT<br>
+      $ ${finalBill}`
+    } else {
+      tipAmount.innerHTML = `TIP AMOUNT<br>
+      $ ${finalBill} <br>
+      EACH`}
   }
 })
-
-
 modalBtn.addEventListener("click", function () {
   modalBg.classList.remove("bg-active");
   container.style.backgroundColor = "white";
