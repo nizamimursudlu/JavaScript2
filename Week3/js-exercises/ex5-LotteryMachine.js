6ea081f1ed1013e1c5266b5674d84d7832b12f28
Nizami Mursudlu <nizamimursudlu@gmail.com>
2020-12-17 14:07:18 +0100

    ex05 improved

--- Week3/js-exercises/ex5-LotteryMachine.js
+++ Week3/js-exercises/ex5-LotteryMachine.js
@@ -1,17 +1,25 @@
 
-
-
-// THE FIRST VERSION
-// CALLS: sayThree, sayThree, sayFive, sayFive
-
-
-
+status = "noPause"
 function threeFive(startIndex, stopIndex, threeCallBack, fiveCallBack) {
   const numbers = [];
   for (i = startIndex; i <= stopIndex; i++) {
     numbers.push(i)
   }
   console.log(numbers)
+  function five() {
+    numbers.map(function (number) {
+      if (number % 5 === 0 && status === "pause") {
+        fiveCallBack(number)
+        status = "noPause"
+      }
+      else
+        if (number % 5 === 0 && status === "noPause") {
+          setTimeout(function () { fiveCallBack(number) }, 1000)
+          status = "pause"
+
+        }
+    })
+  } five()
   function three() {
     numbers.map(function (number) {
       if (number % 3 === 0) {
@@ -19,51 +27,11 @@ function threeFive(startIndex, stopIndex, threeCallBack, fiveCallBack) {
       }
     })
   } three()
-  function five() {
-    numbers.map(function (number) {
-      if (number % 5 === 0) {
-        fiveCallBack(number)
-      }
-    })
-  } five()
 }
 function sayThree(number) {
-  console.log(number)
+  console.log("sayThree")
 }
 function sayFive(number) {
-  console.log(number)
+  console.log("sayFive")
 }
 threeFive(10, 15, sayThree, sayFive)
-
-
-// SECOND VERSION
-// CALLS: sayFive, sayThree, sayThree
-
-
-function threeFive(startIndex, stopIndex, threeCallBack, fiveCallBack) {
-  const numbers = [];
-  for (i = startIndex; i <= stopIndex; i++) {
-    numbers.push(i)
-  }
-  console.log(numbers)
-  numbers.map(function (number) {
-    if (number % 3 === 0) {
-      threeCallBack(number)
-    } else if (number % 5 === 0) {
-      fiveCallBack(number)
-    }
-  })
-}
-function sayThree(number) {
-  console.log(number)
-}
-function sayFive(number) {
-  console.log(number)
-}
-threeFive(10, 15, sayThree, sayFive)
-
-
-// HELLO I WROTE TWO VERSIONS NEITHER ONE CALLS:
-// sayFive, sayThree, sayThree, sayFive
-// AS THE ASSINGMENT ASKS
-// COULD YOU PLEASE GIVE ME A HINT
